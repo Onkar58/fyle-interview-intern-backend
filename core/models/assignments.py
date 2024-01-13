40e8a23c6635de59663dfa48fa7e6dc945fadcd8
Onkar58 <onkarwaghmode58@gmail.com>
2024-01-14 00:45:22 +0530

    2 failed 16 success

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -79,8 +79,10 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
-        assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'If an assignment is in Draft state, it cannot be graded by principal')
-
+        if auth_principal.principal_id is not None:
+            assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'If an assignment is in Draft state, it cannot be graded by principal')
+        if auth_principal.teacher_id is not None:
+            assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'only a submitted assignment can be graded')
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
         db.session.flush()
